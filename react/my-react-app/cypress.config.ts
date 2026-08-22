1c858685689bdce4823015a187152b475d1a7a7e
Prabhakar Laini <prabhakar.laini.pg@gmail.com>
2026-08-22 22:45:19 +0530

    default configs, retry mechanism, failure with timeout

--- react/my-react-app/cypress.config.ts
+++ react/my-react-app/cypress.config.ts
@@ -5,5 +5,9 @@ export default defineConfig({
     baseUrl: 'http://localhost:5173',
     specPattern: 'cypress/e2e/**/*.cy.{js,jsx,ts,tsx}',
     supportFile: 'cypress/support/e2e.ts',
+    /*defaultCommandTimeout: 6000, // default 4000ms, i.e 4 seconds
+    requestTimeout: 10_000,  //5000 by default
+    responseTimeout: 30_000,
+    pageLoadTimeout: 60_000,*/
   },
 })
--- react/my-react-app/cypress/e2e/authentication.cy.ts
+++ react/my-react-app/cypress/e2e/authentication.cy.ts
@@ -2,12 +2,16 @@ describe('Application access', () => {
   beforeEach(() => {
     cy.clearLocalStorage()
   })
-
+ /* cy.get('input')       // Query: retried
+      .should('be.visible') // Assertion: retried
+      .type('hello') //Action: Execute once */
   it('redirects an unauthenticated visitor to login', () => {
     cy.visit('/')
 
     cy.location('pathname').should('equal', '/login')
     cy.contains('h1', 'Welcome back').should('be.visible')
+    //If the input element doesn’t exist yet, Cypress repeats the query and assertion until they pass or time out.
     cy.get('input[name="email"]').should('be.visible').and('be.enabled')
+    cy.get('button[type="submit"]').should('be.visible').and('be.enabled');
   })
 })
